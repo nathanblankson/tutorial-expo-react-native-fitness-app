18dd6a1531bf5ba42b918c39a5d292dae7c6a65e
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-24 21:49:02 +0100

    feat: add auth routes

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,9 +1,34 @@
+import { useAuth } from '@clerk/clerk-expo';
 import { Stack } from 'expo-router';
+import { ActivityIndicator, View } from 'react-native';
 
 const Layout = () => {
+    const {
+        isLoaded,
+        isSignedIn,
+        userId,
+        sessionId,
+        getToken,
+    } = useAuth();
+
+    if (!isLoaded) {
+        return (
+            <View className="flex-1 justify-center items-center">
+                <ActivityIndicator size="large" color="#0000ff"/>
+            </View>
+        )
+    }
+
     return (
         <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index"/>
+            <Stack.Protected guard={isSignedIn}>
+                <Stack.Screen name="(tabs)"/>
+            </Stack.Protected>
+
+            <Stack.Protected guard={!isSignedIn}>
+                <Stack.Screen name="sign-in"/>
+                <Stack.Screen name="sign-up"/>
+            </Stack.Protected>
         </Stack>
     )
 }
