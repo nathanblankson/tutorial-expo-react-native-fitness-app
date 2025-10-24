aad34d07956cb092585598b654b97555d02f8a19
nathanblankson <nathanblankson@hotmail.co.uk>
2025-10-24 21:19:35 +0100

    feat: use token cache in clerk provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,11 @@
 import '../global.css';
 import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Slot } from 'expo-router';
 
 export default function Layout() {
     return (
-        <ClerkProvider>
+        <ClerkProvider tokenCache={tokenCache}>
             <Slot
                 screenOptions={{
                     headerShown: false,
